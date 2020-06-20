soraxas/echo360

commit a3a675328bef1b9f84d3f2aa45fbd2b23bcaeecb

fix gevent patch in py3.6

echo360/hls_downloader.py
@@ -1,6 +1,3 @@
-from gevent import monkey
-monkey.patch_all()
-from gevent.pool import Pool
 import ffmpy
 import gevent
 import requests

echo360/main.py
@@ -232,6 +232,10 @@ def main():
                                 course_url).group()  # retrieve the last part of the URL
         course = EchoCloudCourse(course_uuid, course_hostname)
     else:
+        # import it here for monkey patching gevent, to fix the followings:
+        # MonkeyPatchWarning: Monkey-patching ssl after ssl has already been 
+        # imported may lead to errors, including RecursionError on Python 3.6.
+        from . import hls_downloader
         course_uuid = re.search('[^/]+(?=/$|$)',
                                 course_url).group()  # retrieve the last part of the URL
         course = EchoCourse(course_uuid, course_hostname)

setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     platforms='linux, macos, windows',
     entry_points={
     'console_scripts': [
-        'echo360download=echo360.main:main',
+        'echo360-download=echo360.main:main',
         ],
         },
     package_data={'echo360': ['*.py']},

commit a6aeec39afae1e3c0b574c7e55af557026fd9566

fix broken changes introduced in a3a6753

echo360/hls_downloader.py
@@ -1,5 +1,6 @@
 import ffmpy
 import gevent
+from gevent.pool import Pool
 import requests
 import os, sys
 import time